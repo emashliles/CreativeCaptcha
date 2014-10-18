c12310256b1355536d9c19d811f73da01f97d96f
Chris Goosey <chris@dfg.gd>
2014-10-18 16:27:41 +0100

    Added fail clause to fake backend

--- js/creativeCaptcha.js
+++ js/creativeCaptcha.js
@@ -2,8 +2,13 @@ $(document).ready(function() {
     console.log("I'm alive!");
     //Locate the form closest to the Captcha div
     $("#myCaptcha").parents("form").css("background", "yellow");
+
     // Request an image to trace
     $.get('/captcha/basic', function(data) {
         $(captcha).html('<img src="'+ data + '">');
+    })
+    // Add a fail catch because we can't talk to the back end
+    .fail(function() {
+        $(captcha).html('<img width="300" height="200" src="Images/BasicImages/House.png">');
     });
 });
