3649f6449966013523134d84040c4ac158429dc1
Eleonore <eleonore.mayola@gmail.com>
2014-10-18 17:25:13 +0100

    Creating a list of objects with mouse positions

--- js/creativeCaptcha.js
+++ js/creativeCaptcha.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     console.log("I'm alive!");
-    //Locate the form closest to the Captcha div
+    // Locate the form closest to the Captcha div
     $("#myCaptcha").parents("form").submit(function(event) {
     event.preventDefault();
     console.log("Hello!");
@@ -13,4 +13,28 @@ $(document).ready(function() {
     .fail(function() {
         $(captcha).html('<img width="300" height="200" src="Images/BasicImages/House.png">');
     });
+    // Detect mouse movements
+        var mouseMovements = [];
+        var position;
+    $('html').mousemove(function(event){
+        position = {
+            x: event.pageX,
+            y: event.pageY
+        };
+        //console.log(position.x);
+        mouseMovements.push(position);
+        console.log(mouseMovements);
+        // "Draw" the outline from mouse movement
+        var color = 'red';
+        var size = '2px';
+        $("body").append(
+            $('<div></div>')
+                .css('position', 'absolute')
+                .css('top', event.pageY + 'px')
+                .css('left', event.pageX + 'px')
+                .css('width', size)
+                .css('height', size)
+                .css('background-color', color)
+        );
+    }); 
 });
