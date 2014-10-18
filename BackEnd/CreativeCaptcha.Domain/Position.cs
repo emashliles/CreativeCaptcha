2a0d01a7023c93c2ef9a4b4e9bfa38215d92a278
emashliles <emashliles@gmail.com>
2014-10-18 17:26:38 +0100

    added position.cs

---
+++ BackEnd/CreativeCaptcha.Domain/Position.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreativeCaptcha.Domain
+{
+   
+
+   public class Position
+    {
+        public int XCoordinate { get; set; }
+        public int YCoordinate { get; set; }
+
+       public Position(int x, int y)
+       {
+    
+          XCoordinate = x;
+          YCoordinate =y;
+       }
+    }
+}
