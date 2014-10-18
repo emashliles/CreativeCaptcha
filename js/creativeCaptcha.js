e9b0a759ebfce34b850a5c9adf96ab5af48dafa5
Chris Goosey <chris@dfg.gd>
2014-10-18 16:15:28 +0100

    Added get request to load captcha image

--- js/creativeCaptcha.js
+++ js/creativeCaptcha.js
@@ -1,8 +1,9 @@
-
 $(document).ready(function() {
     console.log("I'm alive!");
     //Locate the form closest to the Captcha div
     $("#myCaptcha").parents("form").css("background", "yellow");
-
+    // Request an image to trace
+    $.get('/captcha/basic', function(data) {
+        $(captcha).html('<img src="'+ data + '">');
+    });
 });
-
